30afedad95102043208f9a8baf85774352dce4d7
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2023-12-18 19:17:51 +0100

    Exercicis 01, 02 i 03

--- src/films.js
+++ src/films.js
@@ -1,43 +1,65 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
-}
+  let result = array.map((movie) => movie.director);
+    console.log("Exercice 1", result);
+    return result;
+  }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter(movie =>  movie.director === director);
+  console.log("Exercice 2", result);
+  return result;
 }
+
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  // extraigo las peliculas de un director determinado
+  let moviesDirector = array.filter(movie => movie.director === director);
+   // Si no se encuentran películas para el director, imprime un mensaje y devuelve 0.
+   if (moviesDirector.length === 0) {
+    console.log("EXERCISE 3 -> No movies found for the director");
+    return 0;
+  }
+
+  // con reduce sumo el total de puntuaciones
+  const totalAverage = moviesDirector.reduce((suma, movie) => suma + movie.score, 0);
+
+  // obtengo el promedio dividiendo el total entre las películas existentes y lo convierto en un número con dos decimales
+  let promedio = totalAverage / moviesDirector.length;
+  console.log("Exercice 3", promedio.toFixed(2));
+  return parseFloat(promedio.toFixed(2));
 }
 
-// Exercise 4:  Alphabetic order by title 
+// Exercise 4: Alphabetic order by title
 function orderAlphabetically(array) {
-  
+  let result = array.map((movie) => movie.title);
+
 }
+
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(array) {
 
 }
+
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+function moviesAverageByCategory(array, category) {
 
 }
+
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
+function hoursToMinutes(array) {
 
 }
 
+
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
+function bestFilmOfYear(array, year) {
+
 }
 
 
